e882bca9cc406b040aef12012cfca020eb717a24
CMGConseil <36569113+ActurialCapital@users.noreply.github.com>
2024-06-26 17:30:24 +0200

    Removed fit in BaseTransformer

--- blocks/transformers.py
+++ blocks/transformers.py
@@ -123,28 +123,6 @@ class BaseTransformer(ABC, OneToOneFeatureMixin, FunctionTransformer):
             if key is None:
                 raise ValueError(f"Missing {kw_args} to compute {selected}.")
 
-    @register_feature_names
-    def fit(self, X, y, **kwargs) -> "BaseTransformer":
-        """
-        Fit the underlying estimator on training data `X` and `y`.
-
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Training data.
-        y : array-like of shape (n_samples,)
-            Target values.
-        **kwargs : dict
-            Additional keyword arguments passed to the `fit` method of the 
-            underlying estimator.
-
-        Returns
-        -------
-        self : BaseTransformer
-            The fitted transformer.
-        """
-        return self
-
     @abstractmethod
     def __call__(self, X: AnyArray, y: AnyArray = None) -> AnyArray:
         pass
